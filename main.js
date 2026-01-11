eccd1ac1b932a87f4b582bf0e3d5127057dfb0de
Gopi Vinay <gopivinay@Gopis-MacBook-Pro.local>
2026-01-11 15:51:29 +0530

    added semi color to the sentence visual to the user

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-export const generateText = () => {
+const generateText = () => {
   return 'this is working or not';
 }
 
-export const evaluateTypingTest = async(sentence) => {
+const color = (char) => '\x1b[2m' + char + '\x1b[0m';
+
+const evaluateTypingTest = async(sentence) => {
   const decoder = new TextDecoder();
   const reader = Deno.stdin.readable.getReader();
   let index = 0;
   while (index < sentence.length) {
     const {value,done} = await reader.read();
     if (decoder.decode(value) === sentence[index]) {
-      console.log(sentence[index]);
-      
+      console.clear();
+      console.log(color(sentence.slice(0,index + 1)) + sentence[index + 1] + color(sentence.slice(index + 2)));
       index++;
     }
   }
@@ -19,7 +21,6 @@ export const evaluateTypingTest = async(sentence) => {
 const main = () => {
 const sentence = generateText();
 console.log(sentence);
-
 Deno.stdin.setRaw(true, {cbreak : true});
 evaluateTypingTest(sentence);
 }
